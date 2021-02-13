1a86c4d9d1fcbf19c5579fbe7df5b48e9350d9fb
misuko1999 <hhq85@gmail.com>
2021-02-14 06:10:43 +0700

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = ['Go to market', 'Buy food', 'Make dinner'];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='App'>
+			<header className='App-header'>
+				{todos.map((todo) => (
+					<li>{todo}</li>
+				))}
+			</header>
+		</div>
+	);
 }
 
 export default App;
